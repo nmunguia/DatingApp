a354ffcdfe14dc63a25815ab63ee5ee1b463e7de
DESKTOP-B0UQ5FP\NM <azure@nuevoshorizontes.com>
2019-12-24 14:38:52 -0600

    End of Section#10

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -4,25 +4,29 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: 'Bearer ' + localStorage.getItem('token')
-  })
-};
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     Authorization: 'Bearer ' + localStorage.getItem('token')
+//   })
+// };
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
-  baseUrl = environment.apiUrl;
+  export class UserService {
+    baseUrl = environment.apiUrl;
 
-constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-getUsers(): Observable<User[]> {
-  return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
-}
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl + 'users');
+  }
 
-getUser(id): Observable<User> {
-  return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
-}
+  getUser(id): Observable<User> {
+    return this.http.get<User>(this.baseUrl + 'users/' + id);
+  }
+
+  updateUser(id: number, user: User) {
+    return this.http.put(this.baseUrl + 'users/' + id, user);
+  }
 }
